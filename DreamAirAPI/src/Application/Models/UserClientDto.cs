c6aa6273109b0e6f1e155f190958ed6dac67c957
Depeex <fdepeex@gmail.com>
2024-10-17 15:25:27 -0300

    borrar

---
+++ DreamAirAPI/src/Application/Models/UserClientDto.cs
@@ -0,0 +1,42 @@
+﻿using Application.Services;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class UserClientDto
+    {
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public string nationality { get; set; }
+        public int dni { get; set; }
+        public int phone { get; set; }
+        public int age { get; set; }
+        public List<TicketDto> tickets { get; set; } = new List<TicketDto>();
+
+        public static UserClientDto Create(UserClient userClient) 
+        {
+            var listMapped = userClient.tickets.Select(tck =>
+            {
+                return TicketDto.Create(tck);
+
+                
+            });
+            return new UserClientDto
+            {
+                name = userClient.name,
+                lastName = userClient.lastName,
+                nationality = userClient.nationality,
+                dni = userClient.dni,
+                phone = userClient.phone,
+                age = userClient.age,
+                tickets = listMapped.ToList()
+            };
+        }
+
+    }
+}
